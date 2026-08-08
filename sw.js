e2cb774410b3b7eb199f442956b5bf3e778c484d
andrewrush <andrewrush@users.noreply.github.com>
2026-08-08 18:36:38 +0000

    deploy: 01d2979e09cfaf10906fd226f4bba4d20f27199e

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for WASM Video Player
-const CACHE_NAME = 'wasm-video-player-v6';
+const CACHE_NAME = 'wasm-video-player-v7';
 const FILES_TO_CACHE = [
     './',
     './index.html',
