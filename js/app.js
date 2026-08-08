23c9ea912ae7b823307e69dcca4daf83bc997946
andrewrush <8232067+andrewrush@users.noreply.github.com>
2026-08-08 20:41:04 +0500

    fix: switch ffmpeg wasm cdn from unpkg to jsdelivr

--- js/app.js
+++ js/app.js
@@ -75,8 +75,8 @@ async function init() {
         });
 
         await state.ffmpeg.load({
-            coreURL: 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js',
-            wasmURL: 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm'
+            coreURL: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js',
+            wasmURL: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm'
         });
 
         state.ffmpegReady = true;
