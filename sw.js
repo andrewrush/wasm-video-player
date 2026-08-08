fbb6e3aaaa913d94968eafc94994faece1a75f39
andrewrush <8232067+andrewrush@users.noreply.github.com>
2026-08-09 03:19:59 +0500

    debug: detailed logging in streamingPipeline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for WASM Video Player
-const CACHE_NAME = 'wasm-video-player-v20';
+const CACHE_NAME = 'wasm-video-player-v21';
 const FILES_TO_CACHE = [
     './',
     './index.html',
